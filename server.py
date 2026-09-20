de0e6cdfa786506cf7e90d7acf0861fc8f4c9379
kvsp7 <kvsp7.ai@gmail.com>
2026-09-20 12:30:52 +0530

    BASE-SERVER

--- server.py
+++ server.py
@@ -10,25 +10,26 @@ server_socket.bind(("0.0.0.0", 8080))
 print("Server bound successful!", server_socket)
 
 server_socket.listen(5)
+while True:
+    client_socket, client_address = server_socket.accept()
+    print(f"Client Connected :{client_address}")
 
-client_socket, client_address = server_socket.accept()
-print(f"Client Connected :{client_address}")
+    data = client_socket.recv(1024)
+    print(f"Data Received :{data}")
 
-data = client_socket.recv(1024)
-print(f"Data Received :{data}")
+    #send_data = "Hey, I sent you this.".encode() #output : b"Hey, I sent you this."
+    body = "<h1>Hello from Server<h1>"
+    response = (
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type:text/html\r\n"
+        f"Content-Length:{len(body.encode())}\r\n"
+        "\r\n"
+        f"{body}"
+    )
+    client_socket.sendall(response.encode())
+    print("Data sent successfully!")
 
-#send_data = "Hey, I sent you this.".encode() #output : b"Hey, I sent you this."
-body = "<h1>Hello from Server<h1>"
-response = (
-    "HTTP/1.1 200 OK\r\n"
-    "Content-Type:text/html\r\n"
-    f"Content-Length:{len(body.encode())}\r\n"
-    "\r\n"
-    f"{body}"
-)
-client_socket.sendall(response.encode())
-print("Data sent successfully!")
-
-client_socket.close()
+    client_socket.close()
+    
 server_socket.close()
 
