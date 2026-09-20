07c431e6baef48dc1ebbc686edb4ddc8c2b38f0b
kvsp7 <kvsp7.ai@gmail.com>
2026-09-20 16:51:25 +0530

    problem-1-parsing-request

--- server.py
+++ server.py
@@ -14,15 +14,22 @@ while True:
     client_socket, client_address = server_socket.accept()
     print(f"Client Connected :{client_address}")
 
-    data = client_socket.recv(1024)
-    print(f"Data Received :{data}")
+    data_received = client_socket.recv(1024)
+    print(f"Data Received :{data_received}")
+
+    data_received_decoded = data_received.decode()
+    data = data_received_decoded.split("\r\n")
+    parse_request = data[0].split()
+    print(parse_request)
+
 
     #send_data = "Hey, I sent you this.".encode() #output : b"Hey, I sent you this."
     body = "<h1>Hello from Server<h1>"
+    body_bytes = body.encode()
     response = (
         "HTTP/1.1 200 OK\r\n"
         "Content-Type:text/html\r\n"
-        f"Content-Length:{len(body.encode())}\r\n"
+        f"Content-Length:{len(body_bytes)}\r\n"
         "\r\n"
         f"{body}"
     )
@@ -30,6 +37,6 @@ while True:
     print("Data sent successfully!")
 
     client_socket.close()
-    
-server_socket.close()
+
+#server_socket.close()
 
