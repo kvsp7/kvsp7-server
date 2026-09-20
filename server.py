b33852eefd31368d8e66b7c900b73300b7627300
kvsp7 <kvsp7.ai@gmail.com>
2026-09-20 12:05:15 +0530

    Server-primary-base-single-connection

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 import socket
 
-server_socket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_socket.bind(("::", 8080))
+server_socket.bind(("0.0.0.0", 8080))
+
+# server_socket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+
+# server_socket.bind(("::", 8080))
 print("Server bound successful!", server_socket)
 
 server_socket.listen(5)
@@ -13,8 +17,16 @@ print(f"Client Connected :{client_address}")
 data = client_socket.recv(1024)
 print(f"Data Received :{data}")
 
-send_data = "Hey, I sent you this.".encode() #output : b"Hey, I sent you this."
-client_socket.sendall(send_data)
+#send_data = "Hey, I sent you this.".encode() #output : b"Hey, I sent you this."
+body = "<h1>Hello from Server<h1>"
+response = (
+    "HTTP/1.1 200 OK\r\n"
+    "Content-Type:text/html\r\n"
+    f"Content-Length:{len(body.encode())}\r\n"
+    "\r\n"
+    f"{body}"
+)
+client_socket.sendall(response.encode())
 print("Data sent successfully!")
 
 client_socket.close()
